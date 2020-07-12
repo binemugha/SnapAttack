d456eaf6d34b8a93e29b30113c8c9763e22857ef
Benjamin Inemugha <bennyblasco@gmail.com>
2020-07-12 20:56:07 +0100

    Setting up ViewController

--- SnapAttack/ViewController.swift
+++ SnapAttack/ViewController.swift
@@ -15,12 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCard: UIImageView!
     
     @IBOutlet weak var startGameButton: UIButton!
+    
+    var timer = Timer()
+    var cardTimer = Timer()
+    var timerInt = 20
+    var scoreInt = 0
+    var gameMode = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func startGameButtonWasPressed(_ sender: Any) {
+        print("Button Pressed")
+        firstCard.image = UIImage(named: "2_of_diamonds")
+        secondCard.image = UIImage(named: "5_of_clubs")
     }
     
 }
